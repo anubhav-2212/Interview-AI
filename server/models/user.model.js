f00f5e9fe1c355554d68b7efbab0a3404cc1e463
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-03-29 22:21:14 +0530

    User Model added

---
+++ server/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "Username already exists"],
+        required: [true, "Username is required"]
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
