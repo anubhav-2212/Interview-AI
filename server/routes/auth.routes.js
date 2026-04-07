3dd05f99c3ed04689321d7ac892290e36d5ca19f
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-04-07 22:15:40 +0530

    authmiddleware  route added

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, getMe } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register);
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
+authRoutes.get("/me", authMiddleware, getMe);
+
 
 export default authRoutes;
