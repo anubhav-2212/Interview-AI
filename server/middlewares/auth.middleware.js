01d0cd0d828e14f5457f1d27742a35d2a491a46b
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-04-07 22:11:09 +0530

    authmiddleware  modified

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,11 +1,17 @@
 import jwt from jsonwebtoken;
+import BlackListToken from "../models/blackListToken.model.js";
 
 export const authMiddleware = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: "Token Not found" });
     }
+
     try {
+        const isBlacklisted = await BlackListToken.findOne({ token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: "Invalid Token" });
+        }
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedToken;
         next();
