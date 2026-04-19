a108c1309bb4e3f57821f4a570e3103e663105d5
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-04-20 04:06:16 +0530

    feat: implement ProtectedRoute and PublicRoute wrappers to handle authentication-based navigation logic

--- client/src/app.routes.jsx
+++ client/src/app.routes.jsx
@@ -3,24 +3,26 @@ import Login from "./features/auth/Pages/Login.jsx";
 import Register from "./features/auth/Pages/Register.jsx";
 import Home from "./features/Interview/Pages/Home.jsx";
 import InterviewAIReport from "./features/Interview/Pages/Interview.jsx";
+import ProtectedRoute from "./features/auth/Components/Protected.routes.jsx";
+import PublicRoute from "./features/auth/Components/Public.routes.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/login",
-        element: <Login />,
+        element: <PublicRoute><Login /></PublicRoute>,
     },
     {
         path: "/register",
-        element: <Register />,
+        element: <PublicRoute><Register /></PublicRoute>,
     },
     {
         path: "/",
-        element: <Home />
+        element: <ProtectedRoute><Home /></ProtectedRoute>
 
     },
     {
         path: "/interview",
-        element: <InterviewAIReport />
+        element: <ProtectedRoute><InterviewAIReport /></ProtectedRoute>
     }
 
 ]);
