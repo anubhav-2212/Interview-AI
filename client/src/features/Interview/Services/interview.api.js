1912be4fca47a38ae9d39c092824f9d9a34f563c
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2026-04-28 21:42:58 +0530

    fix: update cookie sameSite attribute to none and point API base URL to production server

--- client/src/features/Interview/Services/interview.api.js
+++ client/src/features/Interview/Services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8001/api/v1/interview",
+  baseURL: "https://interview-ai-ackend.onrender.com/api/v1/interview",
   withCredentials: true,
 });
 /**
